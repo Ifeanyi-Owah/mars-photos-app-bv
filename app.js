62ef75f230d73981c329d7fa0a552a852636c8fe
Ifeanyi-Owah <ifeanyi_owah@yahoo.com>
2019-06-25 23:11:51 -0400

    working backend api

--- app.js
+++ app.js
@@ -10,17 +10,13 @@ var express = require("express"),
 
       app.use(cors());
 
+    // var s = 1000;
+    // var cam = "mast";
   
-   
-    // app.post("/search", (req, res)=>{
-    //   if(req.body.sol !==" " && req.body.camera !==" " ){
-    //      res.redirect("/handleApi");
-    //   }
-     
-    // })
     
-    app.get("/handleApi", (req, res)=>{
-       axios.get("https://mars-photos.herokuapp.com/api/v1/rovers/Curiosity/photos?sol=1000")
+    app.post("/handleApi", (req, res)=>{
+    let {sol, camera} = req.body;
+       axios.get(`https://mars-photos.herokuapp.com/api/v1/rovers/Curiosity/photos?sol=${sol}&camera=${camera}`)
         .then(function(response){
             // console.log(response.data);
             res.json(response.data);
@@ -31,6 +27,7 @@ var express = require("express"),
         })
       
     })
+
 
     app.get("/",(req, res)=>{
         res.send("<h1>This is the landing page</h1>");
